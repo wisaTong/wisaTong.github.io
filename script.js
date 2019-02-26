fc7ff44a85e2a4bc8f10899f87b332c9bee3208b
Wisa Powthongchin <wisa.pow@gmail.com>
2019-02-27 03:53:07 +0700

    added mouse tracking animation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.onscroll = () => { scrollMoveV() };
+document.onmousemove = onmousemove;
 
 var currentTop = document.scrollTop;
 
@@ -11,7 +12,7 @@ document.scrollTop = currentTop;
 const hidden = {
   visibility: 'hidden'
   , opacity: 0
-  , transition: 'visibility 0s 0.3s, opacity 0.3s ease-in'
+  , transition: 'visibility 0s .3s, opacity .3s ease-in'
 }
 
 const show = {
@@ -20,7 +21,14 @@ const show = {
   , transition: 'opacity 2s ease-out'
 }
 
-const active = { color: 'var(--accent-red)', transition: '0.2s ease-out' }
+const active = {
+  color: 'var(--accent-red)'
+  , transition: '.2s ease-out'
+}
+
+// values
+const clientWidth = document.documentElement.clientWidth;
+const clientHeight = document.documentElement.clientWidth;
 
 // sections
 const me = document.getElementById('sect-me');
@@ -42,10 +50,12 @@ const exps = [].slice.call(document.getElementsByClassName('exp-container'));
 
 setStyle(navItems[0], active);
 
+// handler
+
 function scrollMoveV() {
   var scroll = document.documentElement.scrollTop;
 
-  if (scroll > 150) {
+  if (scroll > 200) {
     setStyle(document.getElementById('wisa'), hidden);
     setStyle(document.getElementById('about-me'), hidden);
   } else {
@@ -62,15 +72,33 @@ function scrollMoveV() {
       decorators[1].style.top = (tops[1] - scroll / 1.5) + 'px';
       decorators[2].style.top = (tops[2] - scroll / 2.75) + 'px';
     }
+    var opacity = 1 - (scroll / me.offsetHeight);
+    [].slice.call(decorators).forEach(em => em.style.opacity = opacity);
   } else if (scroll < exp.offsetHeight + exp.offsetTop) {
     setStyle(navItems[1], active)
-    var offsetTop = exp.offsetTop - 50;
-    hExp.style.top = (hExpTop + (scroll - offsetTop) / 1.2) + 'px';
+    var offsetTop = exp.offsetTop;
+    hExp.style.top = (hExpTop + (scroll - offsetTop - 50) / 1.2) + 'px';
   } else if (scroll < work.offsetHeight + work.offsetTop) {
     setStyle(navItems[2], active)
   } else if (scroll < contact.offsetHeight + contact.offsetTop) {
     setStyle(navItems[3], active)
   }
+}
+
+function onmousemove(e) {
+  const xMax = 20; // max X to move in px
+  const yMax = 20;
+
+  var middleX = clientWidth / 2;
+  var middleY = clientHeight / 2;
+
+  var dx = (e.pageX - middleX) / middleX;
+  var dy = (e.pageY - middleY) / middleY;
+
+  var moveX = dx * xMax;
+  var moveY = dy * yMax;
+  exps.forEach(em => em.style.left = moveX + 'px');
+  exps.forEach(em => em.style.top = moveY + 'px');
 }
 
 function setStyle(em, style) {
